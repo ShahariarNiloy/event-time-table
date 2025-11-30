d7f7b71f87f8bdb1edbbfcccc810b3e75ef23264
ShahariarNiloy <shahariarniloyblv@gmail.com>
2025-11-30 12:42:19 +0600

    feat: initial commit with vite tailwind and shadcn config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="min-h-screen min-w-screen">
+      {/* <DaysHeader /> */}
+      {/* <EventWindow /> */}
+    </main>
+  );
+}
+
+export default App;
